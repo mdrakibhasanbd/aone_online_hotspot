79b85d7cc105e52601b08884dcf4c668ab021e9c
mdrakibhasanbd <mdrakibhasanbd369@gmail.com>
2024-12-20 11:53:28 +0600

    get api add

--- backend/app/api/v1/address/routes.py
+++ backend/app/api/v1/address/routes.py
@@ -2,12 +2,17 @@ import json
 
 from flask import Blueprint, jsonify, request
 
-from app.api.v1.database.dbcon import address_collection
+from app.api.v1.database.dbcon import address_collection, JSONEncoder
 
 address_bp = Blueprint('address', __name__)
 @address_bp.route('/add_address', methods=['POST'])
 def add_address():
     data = request.get_json()
     print(json.dumps(data, indent=4))
-    # insert_documents(address_collection, [data])
+    address_collection.insert_one(data)
     return jsonify({'message': 'Address added successfully'}), 201
+
+@address_bp.route('/get_address', methods=['GET'])
+def get_address():
+    addresses = list(address_collection.find())
+    return json.dumps(addresses, cls=JSONEncoder)
